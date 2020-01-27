8f0921c4e36729a1cec5e6ecc469d3052682be22
Alex <40795980+AlexProgrammerDE@users.noreply.github.com>
2020-01-27 18:53:11 +0100

    Create license.sh
    Change-type: minor

---
+++ license.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "TASKINFO: Check if the repo has a valid license"
+set -e
+
+set -u
+
+[[ "${DEBUG}" == "false" ]] || set -x
+
+shopt -s nocaseglob
+if ls license*; then
+  echo "Found file 'license*'" 1>&2
+  echo "Running regex test" 1>&2
+  # Only fail if license is present but not one of the specified ones
+  /usr/bin/egrep -zoi '(apache.*2.0|affero general public license)' license*
+  exit
+fi
